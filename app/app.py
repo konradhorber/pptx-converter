fb1a033775f663aafbf4c6f99736ec682f73ca97
Konrad Horber <konrad.horber@gmail.com>
2023-11-08 13:21:07 +0100

    App update to include PDF

--- app/app.py
+++ app/app.py
@@ -1,28 +1,37 @@
 import streamlit as st
 from pptx import Presentation
+from pdfminer.high_level import extract_text
 
-uploaded_file = st.file_uploader("Choose a PowerPoint file")
+uploaded_file = st.file_uploader("Choose a Powerpoint or PDF file to upload")
+
 if uploaded_file is not None:
-    prs = Presentation(uploaded_file)
-    slides = prs.slides
+    filename = uploaded_file.name
 
-    i=0
-    # sort shapes on slide before reading
-    for slide in slides:
-        shapes = slide.shapes
-        positions = []
-        for shape in shapes:
-            positions.append([shape.left, shape.top])
+    if filename.endswith(".pptx"):
+        prs = Presentation(uploaded_file)
+        slides = prs.slides
 
-        shapes_sorted = (x for _,x in sorted(zip(positions, shapes), key = lambda x:(x[0][0], x[0][1])))
+        i=0
+        # sort shapes on slide before reading
+        for slide in slides:
+            shapes = slide.shapes
+            positions = []
+            for shape in shapes:
+                positions.append([shape.left, shape.top])
 
-    # read shapes on slide
-        st.write(f"Slide {i}:")
-        i += 1
-        for shape in shapes_sorted:
-            if not shape.has_text_frame:
-                continue
-            paragraphs = shape.text_frame.paragraphs
-            for paragraph in paragraphs:
-                st.write(paragraph.text)
-            st.write()
+            shapes_sorted = (x for _,x in sorted(zip(positions, shapes), key = lambda x:(x[0][0], x[0][1])))
+
+        # read shapes on slide
+            st.write(f"Slide {i}:")
+            i += 1
+            for shape in shapes_sorted:
+                if not shape.has_text_frame:
+                    continue
+                paragraphs = shape.text_frame.paragraphs
+                for paragraph in paragraphs:
+                    st.write(paragraph.text)
+                st.write()
+
+    if filename.endswith(".pdf"):
+        text = extract_text(uploaded_file)
+        st.write(text)
